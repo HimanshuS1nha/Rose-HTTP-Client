9c97c3222311c18f1075099bb444dfa1b9f1ab7a
Himanshu Sinha <ashusinha1904@gmail.com>
2025-07-01 20:50:42 +0530

    Sidebar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Sidebar from "./components/sidebar";
+
 const App = () => {
   return (
     <main className="flex w-dvw h-dvh">
-      <div className="w-[20%] border-r border-r-gray-300"></div>
+      <div className="w-[20%] border-r border-r-gray-300">
+        <Sidebar />
+      </div>
       <div className="w-[45%] border-r border-r-gray-300"></div>
       <div className="w-[35%] border-r border-r-gray-300"></div>
     </main>
