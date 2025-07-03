60639f7661aec65bd8c865122c5a59dcf3d72905
Himanshu Sinha <ashusinha1904@gmail.com>
2025-07-03 21:41:36 +0530

    UI updated

--- src/components/request-section.tsx
+++ src/components/request-section.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { invoke } from "@tauri-apps/api/core";
 
@@ -12,12 +12,16 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import KeyValueInput from "@/components/key-value-input";
 
 import { requestMethods } from "@/constants/request-methods";
 
 const RequestSection = () => {
   const [requestMethod, setRequestMethod] = useState("get");
   const [url, setUrl] = useState("");
+  const [headers, setHeaders] = useState<[boolean, string, string][]>([
+    [false, "", ""],
+  ]);
 
   const { mutate: handleMakeRequest, isPending } = useMutation({
     mutationKey: [`make-${requestMethod}-request`],
@@ -36,6 +40,29 @@ const RequestSection = () => {
       console.error(error);
     },
   });
+
+  const updateHeaders = useCallback(
+    (index: number, type: "checkbox" | "key" | "value", value?: string) => {
+      setHeaders((prev) => {
+        const newHeaders = prev.map((header, i) => {
+          if (i === index) {
+            if (type === "checkbox") {
+              return [!header[0], header[1], header[2]];
+            } else if (type === "key") {
+              return [header[0], value, header[2]];
+            } else {
+              return [header[0], header[1], value];
+            }
+          } else {
+            return header;
+          }
+        });
+
+        return newHeaders as [boolean, string, string][];
+      });
+    },
+    [headers, setHeaders]
+  );
   return (
     <div className="py-2 flex flex-col gap-y-1.5">
       <form
@@ -104,6 +131,29 @@ const RequestSection = () => {
 
           <div className="w-full h-[0.5px] bg-gray-300 -mt-1" />
         </div>
+
+        <KeyValueInput
+          state={headers}
+          updateState={updateHeaders}
+          title="HTTP Headers"
+          addOne={() => {
+            setHeaders((prev) => {
+              const newHeaders = [...prev, [false, "", ""]] as [
+                boolean,
+                string,
+                string
+              ][];
+
+              return newHeaders;
+            });
+          }}
+          removeOne={(index) => {
+            if (index !== 0) {
+              setHeaders((prev) => prev.filter((_, i) => i !== index));
+            }
+          }}
+          tabValue="headers"
+        />
       </Tabs>
     </div>
   );
