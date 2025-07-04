45fdba5f1d619c47a42d6d86b6699615d35b3471
Himanshu Sinha <ashusinha1904@gmail.com>
2025-07-04 21:14:23 +0530

    UI updated

--- src/components/response-section.tsx
+++ src/components/response-section.tsx
@@ -1,3 +1,5 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const ResponseSection = () => {
   return (
     <div className="px-2 py-3 flex flex-col gap-y-1.5 overflow-y-auto">
@@ -12,6 +14,26 @@ const ResponseSection = () => {
           Time: <span className="text-emerald-600 font-semibold">300 ms</span>
         </p>
       </div>
+
+      <Tabs defaultValue="response">
+        <div className="w-full flex flex-col">
+          <TabsList className="bg-transparent gap-x-4">
+            <TabsTrigger
+              value="response"
+              className="text-gray-700 data-[state=active]:bg-transparent data-[state=active]:text-black data-[state=active]:border-b-3 data-[state=active]:border-b-primary data-[state=active]:shadow-none focus-visible:border-none focus-within:ring-0 rounded-none cursor-pointer hover:text-black"
+            >
+              Response
+            </TabsTrigger>
+            <TabsTrigger
+              value="headers"
+              className="text-gray-700 data-[state=active]:bg-transparent data-[state=active]:text-black data-[state=active]:border-b-3 data-[state=active]:border-b-primary data-[state=active]:shadow-none focus-visible:border-none focus-within:ring-0 rounded-none cursor-pointer hover:text-black"
+            >
+              Headers
+            </TabsTrigger>
+          </TabsList>
+          <div className="w-full h-[0.5px] bg-gray-300 -mt-1" />
+        </div>
+      </Tabs>
     </div>
   );
 };
