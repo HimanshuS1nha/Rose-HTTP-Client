6c489472dc1c30f9571033cac327d198b9ab5104
Himanshu Sinha <ashusinha1904@gmail.com>
2025-07-06 15:17:44 +0530

    Tauri store plugin integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,60 @@
+import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { load } from "@tauri-apps/plugin-store";
+
 import Sidebar from "@/components/sidebar";
 import RequestSection from "@/components/request-section";
-import ResponseSection from "./components/response-section";
+import ResponseSection from "@/components/response-section";
+import Loading from "@/components/loading";
+
+import { useStore } from "@/hooks/use-store";
+import { useRequests } from "@/hooks/use-requests";
 
 const App = () => {
+  const setStore = useStore((state) => state.setStore);
+
+  const getRequests = useRequests((state) => state.getRequests);
+
+  const { data, isPending, error } = useQuery({
+    queryKey: ["load-store"],
+    queryFn: async () => {
+      const store = await load("rose.json", { autoSave: false });
+
+      return store;
+    },
+  });
+
+  useEffect(() => {
+    if (error) {
+      alert("Error in loading previous requests");
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (data) {
+      setStore(data);
+      getRequests(data);
+    }
+  }, [data]);
   return (
     <main className="flex w-dvw h-dvh">
-      <div className="w-[20%] border-r border-r-gray-300">
-        <Sidebar />
-      </div>
-      <div className="w-[45%] border-r border-r-gray-300">
-        <RequestSection />
-      </div>
-      <div className="w-[35%] border-r border-r-gray-300">
-        <ResponseSection />
-      </div>
+      {isPending ? (
+        <div className="flex justify-center items-center w-full h-full">
+          <Loading />
+        </div>
+      ) : (
+        <>
+          <div className="w-[20%] border-r border-r-gray-300">
+            <Sidebar />
+          </div>
+          <div className="w-[45%] border-r border-r-gray-300">
+            <RequestSection />
+          </div>
+          <div className="w-[35%] border-r border-r-gray-300">
+            <ResponseSection />
+          </div>
+        </>
+      )}
     </main>
   );
 };
---
+++ src/components/loading.tsx
@@ -0,0 +1,7 @@
+import { Loader2 } from "lucide-react";
+
+const Loading = () => {
+  return <Loader2 className="text-primary animate-spin" size={30} />;
+};
+
+export default Loading;
